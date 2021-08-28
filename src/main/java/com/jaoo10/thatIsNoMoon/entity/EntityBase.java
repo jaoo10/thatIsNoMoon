a2b9177c81e5987b6d3283030c12ca9ea848ab7f
jaoo10 <j.marcus.faria@gmail.com>
2021-08-28 13:55:36 -0300

    primeiro commit

---
+++ src/main/java/com/jaoo10/thatIsNoMoon/entity/EntityBase.java
@@ -0,0 +1,24 @@
+package com.jaoo10.thatIsNoMoon.entity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+
+@MappedSuperclass
+public class EntityBase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
---
+++ src/test/java/com/jaoo10/thatIsNoMoon/ThatIsNoMoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaoo10.thatIsNoMoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThatIsNoMoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
